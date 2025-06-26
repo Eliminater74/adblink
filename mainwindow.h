8430441053deafa98ef095a02a5a6f8a6b5460c4
jocala <jocala@jocala.com>
2025-06-26 12:30:07 -0400

    version is BETA until release

--- mainwindow.h
+++ mainwindow.h
@@ -416,7 +416,7 @@ private slots:
     const QString vqurl = "http://www.jocala.com/version.txt";
     const QString busypath="/data/local/tmp/adblink/";
     const QString tempdir = "/data/local/tmp/";
-    const QString version = "6.7";
+    const QString version = "BETA";
 
 
     int windowSizeSelector;
